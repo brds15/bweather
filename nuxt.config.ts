a1b877514c9bcf9b049ee2511409f7c0e5fbec8e
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-24 10:21:28 -0300

    feat: add i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,11 +6,16 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
+    '@nuxtjs/i18n',
     '@nuxt/fonts',
     '@nuxtjs/tailwindcss',
     '@nuxt/test-utils/module',
-    '@pinia/nuxt'
+    '@pinia/nuxt',
+    '@nuxtjs/i18n'
   ],
+  i18n: {
+    vueI18n: './i18n.config.ts'
+  },
   fonts: {
     families: [{ name: 'Lato', provider: 'google' }]
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const { setLocale } = useI18n()
+</script>
 
 <template>
   <div>
-    <span data-testid="title">index</span>
+    <ul>
+      <li>
+        <button @click="setLocale('en')">en</button>
+      </li>
+      <li>
+        <button @click="setLocale('fr')">fr</button>
+      </li>
+    </ul>
+    <span data-testid="title">
+      {{ $t('welcome') }}
+    </span>
     <Icon
       name="line-md:moon-alt-to-sunny-outline-loop-transition"
       size="5em"
