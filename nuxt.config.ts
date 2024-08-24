24ce1356423086c2264cb33bc93f9c69c79adfc0
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-24 17:44:30 -0300

    fix: move locales to own folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     '@nuxtjs/i18n'
   ],
   i18n: {
-    vueI18n: './i18n.config.ts'
+    vueI18n: './locales/i18n.config.ts'
   },
   fonts: {
     families: [{ name: 'Lato', provider: 'google' }]
