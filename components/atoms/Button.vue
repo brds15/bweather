6e45a793ead8bd5c279e34cf92c7cdb7827e2d71
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-25 00:08:30 -0300

    feat: create button component

---
+++ components/atoms/Button.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { ButtonHTMLAttributes } from 'vue'
+
+  export default defineComponent({
+    name: 'Button',
+    props: {
+      text: {
+        required: false,
+        type: String
+      },
+      type: {
+        default: 'button',
+        required: false,
+        type: String as PropType<ButtonHTMLAttributes['type']>
+      }
+    },
+    setup(props) {
+      return {
+        props
+      }
+    }
+  })
+</script>
+
+<template>
+  <button class="p-3 border border-gray-400" :type="props.type" @click="$emit('click')">
+    {{ props.text }}
+  </button>
+</template>
