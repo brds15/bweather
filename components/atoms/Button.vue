e54aec6668479e6ab8ea22a3b3c697b86a306e31
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-25 16:37:07 -0300

    feat: add disabled props

--- components/atoms/Button.vue
+++ components/atoms/Button.vue
@@ -1,9 +1,14 @@
 <script lang="ts">
-  import type { ButtonHTMLAttributes } from 'vue'
+  import type { ButtonHTMLAttributes, PropType } from 'vue'
 
   export default defineComponent({
     name: 'Button',
     props: {
+      disabled: {
+        default: false,
+        required: false,
+        type: Boolean
+      },
       text: {
         required: false,
         type: String
@@ -23,7 +28,7 @@
 </script>
 
 <template>
-  <button class="p-3 border border-gray-400" :type="props.type" @click="$emit('click')">
+  <button class="p-3 border border-gray-400 disabled:bg-red-800" :disabled="props.disabled" :type="props.type" @click="$emit('click')">
     {{ props.text }}
   </button>
 </template>
