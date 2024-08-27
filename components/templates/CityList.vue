48c2d9ca7df282379686663256bac729ca21be60
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-27 20:46:49 -0300

    feat: move saved locations to CityList.vue

--- components/templates/CityList.vue
+++ components/templates/CityList.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import useWeatherStore from '~/stores/weather'
+  import useLocationStore from '~/stores/location'
+  import Dropdown from '~/components/molecules/Dropdown.vue'
+
+  const weatherStore = useWeatherStore()
+  const locationStore = useLocationStore()
+</script>
 
 <template>
-  <h1>CityList</h1>
+  <Dropdown text="Favorites">
+    <div
+      class="w-full flex items-center justify-around flex-nowrap"
+      v-for="(item, index) in locationStore.locationsSaved"
+      :key="index"
+    >
+      <span class="cursor-pointer" @click="weatherStore.handleWeatherDataWithHistory(item)">
+        <Icon :name="`flagpack:${item.country}`" size="1.4em" style="color: black" />
+        {{ item.location }}
+      </span>
+      <div class="cursor-pointer" @click="locationStore.removeSaveLocation(item)">
+        <Icon name="ic:baseline-delete-outline" size="1.5em" style="color: black" />
+      </div>
+    </div>
+  </Dropdown>
 </template>
