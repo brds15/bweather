91f24c26ce0a3475c517b4b936e7dd818ffb5a4b
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-28 09:57:38 -0300

    feat: create home route constant

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import CityList from '~/components/templates/CityList.vue'
-  import { LOCALES, LOGO_SRC } from '~/constants'
+  import { LOCALES, LOGO_SRC, ROUTES } from '~/constants'
 
   const { setLocale } = useI18n()
 </script>
@@ -10,7 +10,7 @@
     class="h-full w-full scroll-smooth flex flex-col min-h-screen bg-gradient-to-b from-neutral-50 to-sky-50"
   >
     <div class="bg-sky-900 w-full flex flex-wrap justify-between items-center p-4 md:flex-nowrap">
-      <NuxtLink to="/">
+      <NuxtLink :to="ROUTES.HOME">
         <NuxtImg
           format="webp"
           preload
@@ -19,7 +19,9 @@
           :src="LOGO_SRC"
         />
       </NuxtLink>
-      <div class="w-full flex items-center justify-center gap-5 mt-4 md:w-auto md:mt-0 md:justify-end">
+      <div
+        class="w-full flex items-center justify-center gap-5 mt-4 md:w-auto md:mt-0 md:justify-end"
+      >
         <CityList />
         <ul class="flex flex-row flex-wrap gap-5">
           <li v-for="(locale, index) in LOCALES" :key="index">
--- constants.ts
+++ constants.ts
@@ -6,5 +6,6 @@ export const LOCALES = [
 ]
 
 export const ROUTES = {
+  HOME: '/',
   WEATHER: '/weather'
 }
