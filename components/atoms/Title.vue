0e2cbd75cf9ef12219840fe7c9910b6ea9237cdb
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-29 01:58:48 -0300

    feat: create title component

---
+++ components/atoms/Title.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  const props = defineProps(['text'])
+</script>
+
+<template>
+  <h2 class="text-3xl font-bold text-yellow-600 mt-8 mb-4">
+    {{ props.text }}
+  </h2>
+</template>
