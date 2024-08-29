4b51106f6334e030ba35fe7c76e1ab0773102b85
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-29 10:42:42 -0300

    fix: font height

--- pages/index.vue
+++ pages/index.vue
@@ -22,12 +22,12 @@
   <div class="w-full h-auto flex flex-col items-center justify-around p-6">
     <section class="mt-12">
       <h1
-        class="text-6xl font-semibold text-sky-900 leading-9 z-10 md:text-8xl"
+        class="text-6xl font-semibold text-sky-900 z-10 md:text-8xl md:leading-9"
         data-testid="title"
       >
         {{ $t('titleOne') }}
       </h1>
-      <h2 class="text-6xl font-semibold text-sky-900 leading-9 z-0 md:text-8xl">
+      <h2 class="text-6xl font-semibold text-sky-900 z-0  md:text-8xl md:leading-9">
         {{ $t('titleTwo') }}
         <Icon class="relative top-2" name="fxemoji:whitesunsmallcloud" size="1em" />
       </h2>
