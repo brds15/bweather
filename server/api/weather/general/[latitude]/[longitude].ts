af32334e8881b11e9bb608cc3dfd5349a0dbcf53
Bruno Rodrigues <bruno.brds@hotmail.com>
2024-08-29 03:06:40 -0300

    feat: load current locale to call api

--- server/api/weather/general/[latitude]/[longitude].ts
+++ server/api/weather/general/[latitude]/[longitude].ts
@@ -1,11 +1,14 @@
 import { WeatherData } from '~/types/weather'
+import { parseCookies } from 'h3'
 import convertToCamelResponse from '~/server/utils/interceptors/response'
 import transformerGeneralData from '~/server/utils/transformers/general'
 
-const ENDPOINT = 'data/3.0/onecall'
-const QUERY_PARAMS = '&units=metric&lang=pt_br&exclude=minutely&exclude=alerts'
-
 export default defineEventHandler(async event => {
+  const DEFAULT_LOCALE = 'en'
+  const ENDPOINT = 'data/3.0/onecall'
+  const cookies = parseCookies(event)
+  const currentLocale = cookies['i18n_redirected'] || DEFAULT_LOCALE
+  const QUERY_PARAMS = `&units=metric&lang=${currentLocale}&exclude=minutely&exclude=alerts`
   const runtimeConfig = useRuntimeConfig()
   const latitude = getRouterParam(event, 'latitude')
   const longitude = getRouterParam(event, 'longitude')
